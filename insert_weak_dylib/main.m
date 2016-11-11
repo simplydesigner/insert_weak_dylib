f9bd3e086777b1a4322e7a47d1d89568bcc78b20
simplydesigner <simplydesigner@ya.ru>
2016-11-11 20:38:58 +0300

    Initial Commit

---
+++ insert_weak_dylib/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  insert_weak_dylib
+//
+//  Created by simplydesigner on 11/11/16.
+//  Copyright © 2016 simplydesigner. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
